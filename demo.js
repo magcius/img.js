ca8664d7ddb000fe2246818404e299f813a2f85a
Jasper St. Pierre <jstpierre@mecheye.net>
2013-05-23 20:59:48 -0400

    Move the demo stuff to a new file

---
+++ demo.js
@@ -0,0 +1,33 @@
+(function(exports) {
+
+    function makeCanvas(gif) {
+        var canvas = document.createElement("canvas");
+        canvas.width = gif.lsWidth;
+        canvas.height = gif.lsHeight;
+        return canvas;
+    }
+
+    function paintImageBlock(ctx, gif, img) {
+        var tbl = img.colorTable || gif.colorTable;
+        var imgData = ctx.getImageData(img.left, img.top, img.width, img.height);
+        var i, o = 0, n = img.output.length;
+        for (i = 0; i < n; i++) {
+            var pixel = img.output[i];
+            imgData.data[o++] = tbl[pixel].r;
+            imgData.data[o++] = tbl[pixel].g;
+            imgData.data[o++] = tbl[pixel].b;
+            imgData.data[o++] = 255;
+        }
+        ctx.putImageData(imgData, img.left, img.top);
+    }
+
+    window.addEventListener('load', function() {
+        loadGif('marbles.gif', function(stream, gif) {
+            var canvas = makeCanvas(gif);
+            var ctx = canvas.getContext('2d');
+            paintImageBlock(ctx, gif, gif.images[0]);
+            document.body.appendChild(canvas);
+        });
+    });
+
+})(window);
