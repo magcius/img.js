2ffe965a0fe89848748ff499f821881245d07003
Jasper St. Pierre <jstpierre@mecheye.net>
2013-05-25 14:48:25 -0400

    demo: Support subdirectories

--- demo.js
+++ demo.js
@@ -1,10 +1,12 @@
 (function(exports) {
 
+    var base = location.href.split('/').slice(0, -1).join('/') + '/';
+
     window.addEventListener('load', function() {
         var gifs = ['horse.gif', 'bob.gif', 'earth.gif', 'light.gif'];
         gifs.forEach(function(filename) {
-            var url = location.origin + '/' + filename;
-            var gif = runGif(url);
+            var uri = base + filename;
+            var gif = runGif(uri);
             document.body.appendChild(gif);
         });
     });
