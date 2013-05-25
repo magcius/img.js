0d9246c20e2d4e9b587bab58d43473c8f3787e9b
Jasper St. Pierre <jstpierre@mecheye.net>
2013-05-25 10:53:52 -0400

    Add a new demo containing PTE
    This is probably the only GIF in the world using PTE.

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 (function(exports) {
 
     window.addEventListener('load', function() {
-        loadGif(location.origin + '/earth.gif', function(gif) {
+        loadGif(location.origin + '/bob.gif', function(gif) {
             var gif = runGif(gif);
             document.body.appendChild(gif);
         });
